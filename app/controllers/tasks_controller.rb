17b9aabf0df4af1fe04f45e19c7d05c9768b4b75
Jamie Wright <jamie@brilliantfantastic.com>
2021-10-19 15:39:01 -0400

    Implement the analyzer

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,19 @@
+require "task_sentiment"
+
 class TasksController < ApplicationController
   def new
+  end
+
+  def create
+    analyzer = TaskSentiment::Analyzer.new(params["phrase"])
+    results = analyzer.analyze
+
+    if results["error"]
+      @results = results["error"]["message"]
+    else
+      @results = results["choices"]&.first&.[]("text")
+    end
+
+    render :new
   end
 end
